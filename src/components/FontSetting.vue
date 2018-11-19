c82131f05aeb0cb041b50f3d439f576af9a35eca
t-niiru <t.niiru@gmail.com>
2018-11-19 16:03:37 +0900

    :+1: edit code

--- src/components/FontSetting.vue
+++ src/components/FontSetting.vue
@@ -9,9 +9,9 @@
       </b-field>
       <b-field label="font-weight">
         <b-select @input="submit" v-model="FontSetting.FontWeight">
-          <option value="100" selected>100</option>
+          <option value="100">100</option>
           <option value="300">300</option>
-          <option value="400">400</option>
+          <option value="400" selected>400</option>
           <option value="500">500</option>
           <option value="700">700</option>
           <option value="900">900</option>
